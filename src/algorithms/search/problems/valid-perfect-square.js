be0e500ad1b6adab48c9362c9161d39865a61fc0
devilko0l <nguyen.tcao96@gmail.com>
2022-09-15 23:28:21 +0200

    update binary search problem

---
+++ src/algorithms/search/problems/valid-perfect-square.js
@@ -0,0 +1,16 @@
+var isPerfectSquare = function (num) {
+  let low = 0;
+  let high = num;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (mid * mid == num) {
+      return true;
+    }
+    if (mid * mid > num) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return false;
+};
