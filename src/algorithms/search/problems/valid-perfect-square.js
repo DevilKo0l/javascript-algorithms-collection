589d3f9b320e93a6c5e1321a690cb16f2e4637f2
DevilKo0l <nguyen.tcao96@gmail.com>
2022-09-22 17:30:48 +0200

    Update binary search problem

--- src/algorithms/search/problems/valid-perfect-square.js
+++ src/algorithms/search/problems/valid-perfect-square.js
@@ -3,14 +3,21 @@ var isPerfectSquare = function (num) {
   let high = num;
   while (low <= high) {
     let mid = Math.floor((low + high) / 2);
+
+    // Check if num == mid * mid, return true
     if (mid * mid == num) {
       return true;
     }
+
+    // Decide which half to choose for seeking next: left or right
     if (mid * mid > num) {
+      // Go to the left half of the array
       high = mid - 1;
     } else {
+      // Go to the right half of the array
       low = mid + 1;
     }
   }
+  // If it can not find, out the loop and return false
   return false;
 };
